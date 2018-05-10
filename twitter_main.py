dfd4f2f5d4148a3da51326477de37a4bf50291ee
dfotis <fotis.dil94@gmail.com>
2018-05-10 17:44:54 +0300

    Saving the three most retweeted tweets to json files

--- twitter_main.py
+++ twitter_main.py
@@ -1,5 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
+import time
 import json
 import pandas
 import matplotlib
@@ -15,80 +16,118 @@ def save_to_txt(custom_object, filename):
             line+= "\t"
         f.write(line+"\n")
 
+def save_to_json(custom_object, filename):
+    file = filename
+    try:
+        with open(file) as f:
+            data = json.load(f)
+            for ob in custom_object:
+                data.append(ob)
+            f.close()
 
-def get_user_tweets(user_id, api):
-    timeline = []
+        with open(file, 'w') as f:
+            json.dump(data, f, indent=2)
+            f.close()
+    except (Exception):
+        print("failed to save an entry!")
+
+
+def get_user_tweets(user_id, api, filename):
+    data = {}
+    data['tweets'] = []
     progress = 0
 
-    for status in tweepy.Cursor(api.user_timeline, id = user_id).items():
-        timeline.append(status)
-        progress+=1
-        #print(status._json)
+    for tweet in tweepy.Cursor(api.user_timeline, id=user_id).items():
+        data['tweets'].append(tweet)
+        progress += 1
         print("Fetched "+str(progress)+" out of all timeline items")
 
-    return timeline
+    print("Storing tweets...")
+    storing_tweets(data['tweets'], filename)
+    print("Downloaded {0} tweets".format(progress))
+
+    return data['tweets']
 
 
 
-def extract_three_most_rt_tweets(tweets):
+def extract_three_most_rt_tweets(tweets, filename):
     max1 = max2 = max3 = tweets.pop(0)
     count  = 0
     for tw in tweets:
         count += 1
-        if (tw._json["retweet_count"] >= max1._json["retweet_count"]):
+        if (tw["retweet_count"] >= max1["retweet_count"]):
             max3 = max2
             max2 = max1
             max1 = tw
-        elif (tw._json['retweet_count'] >= max2._json["retweet_count"]):
+        elif (tw['retweet_count'] >= max2["retweet_count"]):
             max3 = max2
             max2 = tw
-        elif (tw._json["retweet_count"] >= max3._json["retweet_count"]):
+        elif (tw["retweet_count"] >= max3["retweet_count"]):
             max3 = tw
 
     print("Count: "+ str(count))
 
-    most_rt_tweets = [max1, max2, max3]
-    print("Max1: %s" % max1._json["retweet_count"])
-    print("Max2: %s" % max2._json["retweet_count"])
-    print("Max3: %s" % max3._json["retweet_count"])
+    most_rt_tweets = []
+    most_rt_tweets.append(max1)
+    most_rt_tweets.append(max2)
+    most_rt_tweets.append(max3)
+
+    with open(filename, 'w+') as f:
+        json.dump(most_rt_tweets, f, indent=2)
+        f.close()
+
+    print("Max1: %s" % max1["retweet_count"])
+    print("Max2: %s" % max2["retweet_count"])
+    print("Max3: %s" % max3["retweet_count"])
 
     return most_rt_tweets
 
 
-def collecting_and_storing_tweets(user_id, user_name, api):
-    print("Collecting tweets...")
-    fake_tweets = get_user_tweets(user_id[0], api)
-    real_tweets = get_user_tweets(user_id[1], api)
-
-    print("Extracting three most retweeted tweets of %s ..." % user_name[0])
-    most_rt_fake_tweets = extract_three_most_rt_tweets(fake_tweets)
-
-    print("Extracting three most retweeted tweets of %s ..." % user_name[1])
-    most_rt_real_tweets = extract_three_most_rt_tweets(real_tweets)
-
-    print("Extraction of tweets completed!")
-
-    print("Writing to files the most retweeted tweets...")
-    for tweet in most_rt_fake_tweets:
+def storing_tweets(tweets, filename):
+    print("Writing to files the tweets...")
+    tweet_list = []
+    for tweet in tweets:
         custom_object = {
-            "id": tweet._json["id_str"],
+            "id": tweet._json["id"],
+            "id_str": tweet._json["id_str"],
             "created_at": tweet._json["created_at"],
             "text": tweet._json["text"],
-            "by_user": tweet._json["user"]["id_str"]
+            "by_user": tweet._json["user"],
+            "retweeted": tweet._json["retweeted"],
+            "retweet_count": tweet._json["retweet_count"]
         }
-        # save_to_json(custom_object, 'most_rt_fake_tweets_file.json')
-        save_to_txt(custom_object, 'most_rt_fake_tweets_file.txt')
+        tweet_list.append(custom_object)
+    save_to_json(tweet_list, filename)
+    #save_to_txt(custom_object, filename)
 
-    for tweet in most_rt_real_tweets:
-        custom_object = {
-            "id": tweet._json["id_str"],
-            "created_at": tweet._json["created_at"],
-            "text": tweet._json["text"],
-            "by_user": tweet._json["user"]["id_str"]
-        }
-        # save_to_json(custom_object, 'most_rt_real_tweets_file.json')
-        save_to_txt(custom_object, 'most_rt_real_tweets_file.txt')
-    print("Writing Completed!")
+
+
+def collecting_tweets(user_id, user_name, api, filename):
+    tweetCount = 0
+    maxTweets = 1000000
+    data = {}
+    data['tweets'] = []
+
+    while tweetCount < maxTweets:
+        print(api.rate_limit_status()['resources']['search'])
+        try:
+            # Write the JSON format to the text file, and add one to the number of tweets we've collected
+            for tweet in tweepy.Cursor(api.user_timeline, id=user_id).items(10):
+                data['tweets'].append(tweet)
+                tweetCount += 1
+
+            print("Storing tweets...")
+            storing_tweets(data['tweets'], filename)
+            data['tweets'] = []
+
+            # Display how many tweets we have collected
+            print("Downloaded {0} tweets".format(tweetCount))
+        except tweepy.TweepError as e:
+            print("some error : " + str(e))
+
+    print("Downloaded {0} tweets, Saved to {1}".format(tweetCount, filename))
+
+
 
 
 
@@ -103,8 +142,7 @@ def initialize():
     auth.set_access_token(access_token, access_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit=True,
-                     wait_on_rate_limit_notify=True,
-                     )
+                     wait_on_rate_limit_notify=True)
 
     return api
 
@@ -116,6 +154,32 @@ def main():
     user_id = ["525815006", "335455570"]
     user_name = ["FolksRtalking", "ReutersWorld"]
 
-    collecting_and_storing_tweets(user_id, user_name, api)
+    with open('fake.json', mode='w', encoding='utf-8') as f:
+        json.dump([], f)
+    with open('real.json', mode='w', encoding='utf-8') as f:
+        json.dump([], f)
+
+    #collecting_tweets(user_id[0], user_name[0], api, 'fake.json')
+    #collecting_tweets(user_id[1], user_name[1], api, 'real.json')
+
+
+    get_user_tweets(user_id[0], api, 'fake.json')
+    get_user_tweets(user_id[1], api, 'real.json')
+
+    print("Extracting three most retweeted tweets of %s ..." % user_name[0])
+    with open('fake.json', mode='r', encoding='utf-8') as f:
+        fake_tweets = json.load(f)
+    most_rt_fake_tweets = extract_three_most_rt_tweets(fake_tweets, 'most_rt_fake_tweets.json')
+
+
+    print("Extracting three most retweeted tweets of %s ..." % user_name[1])
+    with open('real.json', mode='r', encoding='utf-8') as f:
+        real_tweets = json.load(f)
+    most_rt_real_tweets = extract_three_most_rt_tweets(real_tweets, 'most_rt_real_tweets.json')
+
+
+    print("Extraction of tweets completed!")
+
+
 
 main()
